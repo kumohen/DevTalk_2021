0c05cfa9bf7ca64502b404f0932c005809451bd7
Mohen Mondal <37147607+kumohen@users.noreply.github.com>
2021-09-28 17:42:15 +0530

    Update keys.js

--- keys.js
+++ keys.js
@@ -1,6 +1,6 @@
 
 module.exports = {
   MONGO_URI:
-    "mongodb+srv://mahen:12345a@cluster0.417gh.mongodb.net/LINKEDIN_2021?retryWrites=true&w=majority",
+    "mongodb+srv://mahen:.mongodb.net/LINKEDIN_2021?retryWrites=true&w=majority",
   JWT_SECRET: "MOHENMONDALJDFKFLL",
 };
